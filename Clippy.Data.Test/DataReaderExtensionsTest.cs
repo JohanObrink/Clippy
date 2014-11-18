391be8176e8a6dfad4e04d3950a4d85700050507
Johan Öbrink <johan.obrink@gmail.com>
2014-11-18 13:49:01 +0100

    Extensions for DataReader

---
+++ Clippy.Data.Test/DataReaderExtensionsTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data;
+using Clippy.Data;
+using Xunit;
+using FluentAssertions;
+
+namespace Clippy.Data.Test
+{
+    public class DataReaderExtensionsTest
+    {
+        private readonly DataSet ds;
+        private readonly DataTable dt;
+
+        public DataReaderExtensionsTest()
+        {
+            ds = new DataSet();
+            dt = ds.AddTable("nonNullables")
+                .AddColumn<int>("id")
+                .AddColumn<string>("name")
+                .AddColumn<DateTime>("birth")
+                .AddColumn<bool>("alive");
+        }
+
+        [Fact]
+        public void It_can_read_non_nullable_values()
+        {
+            dt.AddData(1, "Johan", new DateTime(1973, 4, 16), true);
+            var reader = ds.CreateDataReader();
+            reader.Read();
+            reader.Value<int>(0).Should().Be(1);
+            reader.Value<string>(1).Should().Be("Johan");
+            reader.Value<DateTime>(2).Should().Be(new DateTime(1973, 4, 16));
+            reader.Value<bool>(3).Should().BeTrue();
+        }
+
+        [Fact]
+        public void It_can_read_nullable_values()
+        {
+            dt.AddData(null, null, null, null);
+            var reader = ds.CreateDataReader();
+            reader.Read();
+            reader.Value<int?>(0).Should().NotHaveValue();
+            reader.Value<string>(1).Should().BeNull();
+            reader.Value<DateTime?>(2).Should().NotHaveValue();
+            reader.Value<bool?>(3).Should().NotHaveValue();
+        }
+
+        [Fact]
+        public void It_throws_if_null_is_found_for_non_nullable_type()
+        {
+            dt.AddData(null, null, null, null);
+            var reader = ds.CreateDataReader();
+            reader.Read();
+            Assert.Throws<ArgumentException>(() => reader.Value<int>(0));
+            Assert.Throws<ArgumentException>(() => reader.Value<DateTime>(2));
+            Assert.Throws<ArgumentException>(() => reader.Value<bool>(3));
+        }
+    }
+}
---
+++ Clippy.Data/DataReaderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+
+namespace Clippy.Data
+{
+    public static class DataReaderExtensions
+    {
+        public static T Value<T>(this IDataReader reader, int index)
+        {
+            var type = typeof(T);
+            var isNull = reader.IsDBNull(index);
+            var val = (isNull) ? null : reader.GetValue(index);
+            try
+            {
+                return (T)(object)val;
+            }
+            catch
+            {
+                throw new ArgumentException(string.Format("Column {0} ({1}) cannot be cast to a {2} with value {2}", index, reader.GetDataTypeName(index), type.Name, val ?? "null"));
+            }
+        }
+    }
+}
